1d3df3557806057b9c603638fa12e417966df785
spencerlelswick <spencerlelswick@gmail.com>
2023-04-26 15:50:04 -0400

    project init

---
+++ src/data/mockdata.js
@@ -0,0 +1,26 @@
+export const people = [
+  {
+    id: 1,
+    name: 'John Rogers',
+    job: 'Application Developer',
+    image:
+      'https://www.shutterstock.com/shutterstock/photos/1032768337/display_1500/stock-photo-innovative-ideas-smart-nice-cheerful-man-sitting-at-the-table-and-looking-for-new-innovative-ideas-1032768337.jpg',
+    text: 'test',
+  },
+  {
+    id: 2,
+    name: 'Roger Smith',
+    job: 'Software Engineer',
+    image:
+      'https://www.shutterstock.com/shutterstock/photos/1032768337/display_1500/stock-photo-innovative-ideas-smart-nice-cheerful-man-sitting-at-the-table-and-looking-for-new-innovative-ideas-1032768337.jpg',
+    text: 'test',
+  },
+  {
+    id: 3,
+    name: 'Amanda Johnson',
+    job: 'UI/UX Designer',
+    image:
+      'https://www.shutterstock.com/shutterstock/photos/1032768337/display_1500/stock-photo-innovative-ideas-smart-nice-cheerful-man-sitting-at-the-table-and-looking-for-new-innovative-ideas-1032768337.jpg',
+    text: 'test',
+  },
+]
